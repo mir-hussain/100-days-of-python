3f057e3009922dd7c4c2b7ec5ee6bd0d7ec53d36
mir-hussain <mirhussainmurtaza@gmail.com>
2023-01-26 13:04:54 +0600

    band name generator added

--- Day-1-band-name-generator/REDME.md
+++ Day-1-band-name-generator/REDME.md
@@ -1,1 +1,9 @@
 # Band name generator
+
+In this exercise you have to take user input mentioned below.
+
+- City he/she grew up in.
+- Name of his/her pet.
+
+After that you have to concat two words to generate a single word as band name.
+
--- Day-1-band-name-generator/script.py
+++ Day-1-band-name-generator/script.py
@@ -1,1 +1,5 @@
-print("Hello")
+print("Welcome to the Band name generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city} {pet_name}")
