c30dcba0ab88408ad66e518deb3b9a333c872e5f
mir-hussain <mirhussainmurtaza@gmail.com>
2023-01-28 02:24:02 +0600

    day 5 random password generator added

--- Day-5-password-generator/script.py
+++ Day-5-password-generator/script.py
@@ -1,1 +1,32 @@
-print("Password Generator")
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?: "))
+nr_symbols = int(input(f"How many symbols would you like?: "))
+nr_numbers = int(input(f"How many numbers would you like?: "))
+
+password_list = []
+
+if nr_letters != 0:
+    for x in range(0, nr_letters):
+        password_list.append(random.choice(letters))
+
+if nr_symbols != 0:
+    for x in range(0, nr_symbols):
+        password_list.append(random.choice(symbols))
+
+if nr_numbers != 0:
+    for x in range(0, nr_numbers):
+        password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = "".join(password_list)
+
+print(f"Generated password is =>  {password}")
