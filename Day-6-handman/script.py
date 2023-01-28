cd3078580ccb4ef0c8e6635c7e26fde450f7cea8
mir-hussain <mirhussainmurtaza@gmail.com>
2023-01-28 14:50:31 +0600

    re-factored for better user experience

--- Day-6-handman/script.py
+++ Day-6-handman/script.py
@@ -1,70 +1,15 @@
+from colorama import init, Fore
 import random
+from hangman_art import logo, stages
+from word_list import word_list
 
-print("Hangman")
+init()
 
-word_list = ["Banana", "Apple", "Mango"]
+print(logo)
 life = 6
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+
 
 selected_word = random.choice(word_list).lower()
-
-print(selected_word)
-
 
 blank_space = []
 
@@ -77,18 +22,21 @@ while "_" in blank_space and life > 0:
 
     user_selection = input("Guess a letter: ").lower()
 
-    if user_selection in selected_word:
+    if user_selection in blank_space:
+        print("You already guessed this letter correctly.")
+    elif user_selection in selected_word:
         for i in range(0, len(selected_word)):
             if user_selection == selected_word[i]:
                 blank_space[i] = user_selection
     else:
+        print(f"There is no {user_selection} in the word. You lost a life.")
         life = life - 1
 
     print("".join(blank_space) + "\n")
     print(stages[life])
 
 if life == 0:
-    print("You lose.")
+    print(Fore.RED + f"You lose. The word was '{selected_word}'")
 
 if not "_" in blank_space:
-    print("You won.")
+    print(Fore.GREEN + "You won.")
