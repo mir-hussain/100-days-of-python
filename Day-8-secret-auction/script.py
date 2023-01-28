f16c49cc61a0faee96e4725ffe3909f4de54c460
mir-hussain <mirhussainmurtaza@gmail.com>
2023-01-29 01:40:18 +0600

    secret auction basic logic added

---
+++ Day-8-secret-auction/script.py
@@ -0,0 +1,36 @@
+import os
+print("Welcome to secret auction.")
+
+participants = {}
+
+should_run = True
+
+
+def find_highest_bidder(data):
+    highest_bid = 0
+    winner = ""
+
+    for name in data:
+        bid = data[name]
+        if bid > highest_bid:
+            highest_bid = bid
+            winner = name
+
+    print(f"The winner is {winner}, with the amount of {highest_bid}$")
+
+
+while should_run:
+    name = input("Enter your name: ")
+    bid = int(input("Enter your bid amount: "))
+    participants[name] = bid
+    choice = input(
+        "Is there anyone else ? type 'yes' and handover, or type 'no' to exit. : ")
+
+    if choice == "no":
+        find_highest_bidder(participants)
+        should_run = False
+    elif choice == "yes":
+        os.system('cls||clear')
+    else:
+        print("Invalid Input")
+        should_run = False
