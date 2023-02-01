37909402505bcc24a558f87028274425c162ce15
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-01 22:21:25 +0600

    input done, working on game logic

--- Day-11-blackjack/script.py
+++ Day-11-blackjack/script.py
@@ -8,7 +8,14 @@ def deal_card():
 
 
 def calculate_card(card_list):
-    return sum(card_list)
+    result = sum(card_list)
+    distance = 21 - result
+
+    return result, distance
+
+
+def display_result(cards, total_score, winner):
+    print(f"{winner}")
 
 
 user_cards = []
@@ -18,7 +25,20 @@ for x in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-user_score = calculate_card(user_cards)
+user_score, user_distance = calculate_card(user_cards)
+computer_score, computer_distance = calculate_card(computer_cards)
 
 print(
     f"Your cards are {user_cards}, total score is {user_score}")
+
+print(f"Computer's first card is {computer_cards[0]}")
+
+# if input("Do you want to draw another card? Type 'y' or 'n'") == "n":
+#     if user_score > 21 and computer_score < 21:
+
+#     if user_distance > computer_distance:
+#         print(
+#             f"Computer cards are {computer_cards}. Total score is {computer_score}")
+#         print("Computer Wins.")
+#     else:
+#         print("You won.")
