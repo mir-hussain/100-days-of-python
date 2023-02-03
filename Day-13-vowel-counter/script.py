0c45f9cb54589089435902af5d30678dad40f0a2
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-04 02:32:42 +0600

    Day 13 vowel meter added

--- Day-13-vowel-counter/script.py
+++ Day-13-vowel-counter/script.py
@@ -13,3 +13,8 @@ character_count = len(text)
 for alphabet in text:
     if alphabet in vowels:
         vowels[alphabet] += 1
+
+for character in vowels:
+    rate = (vowels[character] / character_count) * 100
+    print(
+        f"{character} count = {vowels[character]}, appearance rate = {round(rate)}%")
