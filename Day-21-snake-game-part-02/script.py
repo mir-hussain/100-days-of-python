57ccf9cb4a985c7a11f87b8d7616d64b0b3a41c1
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-10 14:56:25 +0600

    food added

---
+++ Day-21-snake-game-part-02/script.py
@@ -0,0 +1,30 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        print("Nome")
+
+screen.exitonclick()
