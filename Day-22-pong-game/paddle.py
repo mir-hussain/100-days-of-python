b29f8b8c2f7a3f91e00e9689ad7d986d3ba22c28
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-11 02:32:28 +0600

    paddle class added

--- Day-22-pong-game/paddle.py
+++ Day-22-pong-game/paddle.py
@@ -2,13 +2,13 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, x_position):
         super().__init__()
         self.color("white")
         self.shape('square')
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(350, 0)
+        self.goto(x_position, 0)
 
     def paddle_up(self):
         y = self.ycor() + 20
--- Day-22-pong-game/script.py
+++ Day-22-pong-game/script.py
@@ -8,7 +8,8 @@ screen.listen()
 
 screen.tracer(0)
 
-paddle_right = Paddle()
+paddle_right = Paddle(350)
+paddle_left = Paddle(-350)
 
 
 # paddle = Turtle()
@@ -19,18 +20,20 @@ paddle_right = Paddle()
 # paddle.goto(350, 0)
 
 
-def paddle_up():
-    y = paddle_right.ycor() + 20
-    paddle_right.goto(paddle_right.xcor(), y)
+# def paddle_up():
+#     y = paddle_right.ycor() + 20
+#     paddle_right.goto(paddle_right.xcor(), y)
 
 
-def paddle_down():
-    y = paddle_right.ycor() - 20
-    paddle_right.goto(paddle_right.xcor(), y)
+# def paddle_down():
+#     y = paddle_right.ycor() - 20
+#     paddle_right.goto(paddle_right.xcor(), y)
 
 
-screen.onkey(paddle_up, "Up")
-screen.onkey(paddle_down, "Down")
+screen.onkey(paddle_right.paddle_up, "Up")
+screen.onkey(paddle_right.paddle_down, "Down")
+screen.onkey(paddle_left.paddle_up, "w")
+screen.onkey(paddle_left.paddle_down, "s")
 
 game_on = True
 
