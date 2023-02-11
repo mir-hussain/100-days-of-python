c31cf121178433335a32d6a3e435486c3380b629
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-12 02:34:04 +0600

    Turtle movement added

--- Day-23-turtle-run/script.py
+++ Day-23-turtle-run/script.py
@@ -3,7 +3,17 @@ from subject import MyTurtle
 
 screen = Screen()
 screen.setup(height=400, width=600)
+screen.listen()
+screen.tracer(0)
+
 turtle = MyTurtle()
+
+screen.onkey(turtle.move_up, "Up")
+screen.onkey(turtle.move_down, "Down")
+
+game_on = True
+while game_on:
+    screen.update()
 
 
 screen.exitonclick()
