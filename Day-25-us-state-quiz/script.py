4e2f675a40647954404032493cf24ae9a14a82e2
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-15 01:53:22 +0600

    data formatting done

--- Day-25-us-state-quiz/script.py
+++ Day-25-us-state-quiz/script.py
@@ -10,9 +10,19 @@ screen.setup(width=700, height=500)
 screen.addshape(image)
 turtle.shape(image)
 
+state_dict = {}
+state_data = pandas.read_csv("50_states.csv").values
+for state_info in state_data:
+    state_dict[state_info[0]] = (state_info[1], state_info[2])
+
 
 answer = screen.textinput(title="Guess the state",
                           prompt="What's another state name?")
+
+if answer in state_dict:
+    print("Good")
+else:
+    print("Not good")
 
 
 turtle.mainloop()
