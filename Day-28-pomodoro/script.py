4779e96828e02b8d193a6382f3e14f35157a75c5
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-17 02:24:35 +0600

    ui done

--- Day-28-pomodoro/script.py
+++ Day-28-pomodoro/script.py
@@ -22,7 +22,8 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=100, bg=YELLOW)
 
-label = Label(text="Timer", fg=GREEN)
+label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 48), bg=YELLOW)
+label.grid(row=1, column=2)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
@@ -31,4 +32,13 @@ canvas.create_text(100, 130, text="00:00", fill="white",
                    font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=2, column=2)
 
+start_button = Button(text="Start")
+start_button.grid(row=3, column=1)
+
+reset_button = Button(text="Reset")
+reset_button.grid(row=3, column=3)
+
+
+check_label = Label(text="✓", fg=GREEN, font=(FONT_NAME, 48), bg=YELLOW)
+check_label.grid(row=4, column=2)
 window.mainloop()
