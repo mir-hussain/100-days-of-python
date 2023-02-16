0a7bd70b1d00a56bcbf9507e80627985d56d6b3f
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-17 02:15:14 +0600

    basic ui added

---
+++ Day-28-pomodoro/script.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=100)
+
+canvas = Canvas(width=200, height=224)
+tomato_image = PhotoImage(file="tomato.png")
+canvas.create_image(101, 112, image=tomato_image)
+canvas.create_text(101, 130, text="00:00", fill="white",
+                   font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
