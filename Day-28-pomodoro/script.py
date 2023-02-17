41f6e18e851eb142dad04d286991507b95cfc480
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-17 12:55:47 +0600

    check mark added

--- Day-28-pomodoro/script.py
+++ Day-28-pomodoro/script.py
@@ -7,20 +7,65 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
-reps = 1
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        label.config(text="Long Break", fg=RED)
+        countdown(long_break_sec)
+    elif reps % 2 == 0:
+        label.config(text="Short Break", fg=PINK)
+        countdown(short_break_sec)
+    elif reps % 2 == 1:
+        label.config(text="Work", fg=GREEN)
+        countdown(work_sec)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
-# ---------------------------- UI SETUP ------------------------------- #
+def countdown(count):
+    global reps
 
+    minute = math.floor(count / 60)
+    second = count % 60
+
+    if second < 10:
+        second = f"0{second}"
+
+    if minute < 10:
+        minute = f"0{minute}"
+
+    canvas.itemconfig(timer_text, text=f"{minute}:{second}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        session = math.floor(reps/2)
+        for _ in range(session):
+            mark += "✓"
+        check_label.config(text=mark)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
@@ -36,47 +81,6 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
                                 font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=2, column=2)
 
-
-def countdown(count):
-
-    global reps
-
-    minute = math.floor(count / 60)
-    second = count % 60
-
-    if second < 10:
-        second = f"0{second}"
-
-    if minute < 10:
-        minute = f"0{minute}"
-
-    if count == 0:
-        reps += 1
-
-    canvas.itemconfig(timer_text, text=f"{minute}:{second}")
-    if count > 0:
-        print(reps)
-        window.after(1000, countdown, count - 1)
-    else:
-        start_timer()
-
-
-def start_timer():
-
-    global reps
-
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
-
-    if reps % 8 == 0:
-        countdown(long_break_sec)
-    elif reps % 2 == 0:
-        countdown(short_break_sec)
-    elif reps % 2 == 1:
-        countdown(work_sec)
-
-
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=3, column=1)
 
@@ -84,6 +88,6 @@ reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(row=3, column=3)
 
 
-check_label = Label(text="✓", fg=GREEN, font=(FONT_NAME, 24), bg=YELLOW)
+check_label = Label(text="", fg=GREEN, font=(FONT_NAME, 24), bg=YELLOW)
 check_label.grid(row=4, column=2)
 window.mainloop()
