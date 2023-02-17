eb7450b86f906cd5fca3e2321c3118fd4a25ec57
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-17 15:13:43 +0600

    Day 29 password generator added

--- Day-29-password-gen/script.py
+++ Day-29-password-gen/script.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import messagebox
+import random
 
 BACKGROUND_COLOR = "white"
 
@@ -8,8 +10,46 @@ def save_to_file():
     un = username.get()
     pw = password.get()
 
-    with open("pass.txt", "a") as file:
-        file.write(f"{web} | {un} | {pw}\n")
+    if len(web) == 0 or len(un) == 0 or len(pw) == 0:
+        messagebox.showwarning(title="Empty Field",
+                               message="Every filed must be filled out.")
+    else:
+        is_ok = messagebox.askokcancel(
+            title=web, message=f"The information you entered is:\nWebsite:{web}, Email:{un}, Password:{pw}")
+
+        if is_ok:
+            with open("pass.txt", "a") as file:
+                file.write(f"{web} | {un} | {pw}\n")
+                web_input.delete(0, END)
+                password.delete(0, END)
+
+
+def password_gen():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password.delete(0, END)
+
+    password_letters_list = [random.choice(
+        letters) for _ in range(0, random.randint(6, 10))]
+    password_numbers_list = [random.choice(
+        numbers) for _ in range(0, random.randint(4, 6))]
+    password_symbol_list = [random.choice(
+        symbols) for _ in range(0,  random.randint(4, 6)
+                                )]
+
+    password_character_list = password_letters_list + \
+        password_numbers_list + password_numbers_list
+
+    random.shuffle(password_character_list)
+
+    random_password = "".join(password_character_list)
+    print(random_password)
+
+    password.insert(0, random_password)
 
 
 window = Tk()
@@ -46,7 +86,7 @@ password_label.grid(row=4, column=1)
 password = Entry(width=29)
 password.grid(row=4, column=2)
 
-gen_button = Button(text="Generate")
+gen_button = Button(text="Generate", command=password_gen)
 gen_button.grid(row=4, column=3)
 
 # Add
