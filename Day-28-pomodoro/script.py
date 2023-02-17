7c7dca72bfc4f96d106a495c100ae947bccd6bf3
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-17 12:10:18 +0600

    basic count down added

--- Day-28-pomodoro/script.py
+++ Day-28-pomodoro/script.py
@@ -16,7 +16,9 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Pomodoro")
@@ -28,17 +30,28 @@ label.grid(row=1, column=2)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
+                                font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=2, column=2)
 
-start_button = Button(text="Start")
+
+def countdown(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
+
+def start_timer():
+    countdown(5)
+
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=3, column=1)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(row=3, column=3)
 
 
-check_label = Label(text="✓", fg=GREEN, font=(FONT_NAME, 48), bg=YELLOW)
+check_label = Label(text="✓", fg=GREEN, font=(FONT_NAME, 24), bg=YELLOW)
 check_label.grid(row=4, column=2)
 window.mainloop()
