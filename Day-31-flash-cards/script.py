9a8ca3c53c8e1a9d2a7f07ff60c1403ac3e48336
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-18 19:15:15 +0600

    starter added

---
+++ Day-31-flash-cards/script.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+window = Tk()
+window.title("Flash Cards")
+window.config(bg=BACKGROUND_COLOR)
+
+
+window.mainloop()
