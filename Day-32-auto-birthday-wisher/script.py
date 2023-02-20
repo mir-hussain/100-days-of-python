32af22adbaa99835d77d4ee20f110c3a991b9613
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-21 00:32:19 +0600

    Day 32 auto birthday wisher added

--- Day-32-auto-birthday-wisher/script.py
+++ Day-32-auto-birthday-wisher/script.py
@@ -11,20 +11,24 @@ password = os.getenv("password")
 email = os.getenv("email")
 
 
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=email, password=password)
-#     connection.sendmail(
-#         from_addr=email,
-#         to_addrs="mir.hm110@gmail.com",
-#         msg="Subject: Testing ABW \n\n Ami Mir Hussain Bolchi")
-
 birthday_data = pandas.read_csv("./birthday-sheet.csv")
 birthday_list = [{**row}
                  for _, row in birthday_data.iterrows()]
 
-print(birthday_list)
+for info in birthday_list:
+    print(info)
 
-letter_number = random.randint(1, 3)
-with open(f"./letter_templates/letter_{letter_number}.txt") as file:
-    content = file.read()
+    date = dt.datetime.now()
+
+    if date.month == info["month"] and date.day == info["day"]:
+        letter_number = random.randint(1, 3)
+        with open(f"./letter_templates/letter_{letter_number}.txt") as file:
+            content = file.read().replace("[NAME]", info["name"])
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=email, password=password)
+            connection.sendmail(
+                from_addr=email,
+                to_addrs=info["email"],
+                msg=f"Subject: Wishing you Happy Birthday\n\n{content}")
