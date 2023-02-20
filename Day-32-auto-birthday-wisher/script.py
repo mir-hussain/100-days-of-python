00bb47c9caa4ed69599584117ace773abb3fa3dc
Mir Hussain <mir.hm110@gmail.com>
2023-02-20 22:11:01 +0600

    templates added

--- Day-32-auto-birthday-wisher/script.py
+++ Day-32-auto-birthday-wisher/script.py
@@ -1,5 +1,7 @@
 import smtplib
 import os
+import pandas
+import datetime as dt
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,10 +10,17 @@ password = os.getenv("password")
 email = os.getenv("email")
 
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=email, password=password)
-    connection.sendmail(
-        from_addr=email,
-        to_addrs="mir.hm110@gmail.com",
-        msg="Subject: Testing ABW \n\n Ami Mir Hussain Bolchi")
+# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#     connection.starttls()
+#     connection.login(user=email, password=password)
+#     connection.sendmail(
+#         from_addr=email,
+#         to_addrs="mir.hm110@gmail.com",
+#         msg="Subject: Testing ABW \n\n Ami Mir Hussain Bolchi")
+
+birthday_data = pandas.read_csv("./birthday-sheet.csv")
+
+
+with open(f"./letter_templates/letter_{1}.txt") as file:
+    letter = file.read()
+    print(letter)
