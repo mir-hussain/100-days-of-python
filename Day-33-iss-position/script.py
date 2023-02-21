f399e34acf3b3d975f8db0d30c07921cc618f048
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-21 14:42:25 +0600

    sunrise sunset info added

--- Day-33-iss-position/script.py
+++ Day-33-iss-position/script.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
 iss_response.raise_for_status()
@@ -6,7 +7,24 @@ iss_response.raise_for_status()
 iss_position = iss_response.json()
 
 iss_lat = iss_position["iss_position"]["latitude"]
-iss_long = iss_position["iss_position"]["longitude"]
+iss_lng = iss_position["iss_position"]["longitude"]
 
 print(iss_lat)
-print(iss_long)
+print(iss_lng)
+
+params = {
+    "lat": 23.777176,
+    "lng": 	90.399452,
+    "formatted": 0
+}
+
+sun_info = requests.get(
+    url=" https://api.sunrise-sunset.org/json", params=params)
+sun_info.raise_for_status()
+
+sun_data = sun_info.json()
+
+sunrise = sun_data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = sun_data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
