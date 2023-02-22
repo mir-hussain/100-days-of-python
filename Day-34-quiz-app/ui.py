8eb70dd46e288481400c788b86e8f4d7feb306c3
Mir Hussain <mir.hm110@gmail.com>
2023-02-22 13:00:30 +0600

    quiz display que added

--- Day-34-quiz-app/ui.py
+++ Day-34-quiz-app/ui.py
@@ -1,14 +1,18 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface():
-    def __init__(self):
+    def __init__(self, quiz_text: QuizBrain):
+        self.quiz = quiz_text
         self.window = Tk()
         self.window.title("Quiz App")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        # * Score Card
 
         self.score = Canvas(width=100, height=50,
                             bg=THEME_COLOR, highlightthickness=0)
@@ -16,10 +20,12 @@ class QuizInterface():
                                font=("Aria", 18, "normal"), anchor="center")
         self.score.grid(row=1, column=1, padx=0)
 
+        # * Que Card
+
         self.canvas = Canvas(width=300, height=250, highlightthickness=0)
 
-        self.canvas.create_text(150, 125, text="Test ",
-                                anchor="center", font=("Arial", 20, "normal"))
+        self.question = self.canvas.create_text(150, 125, text="Test ",
+                                                anchor="center", font=("Arial", 20, "normal"))
         # self.canvas.create_rectangle(0, 0, 300, 250, fill="white")
         self.canvas.grid(row=2, column=1, columnspan=2, pady=20)
 
@@ -37,4 +43,10 @@ class QuizInterface():
 
         self.true_button.grid(row=3, column=2, pady=20)
 
+        self.next_que()
+
         self.window.mainloop()
+
+    def next_que(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question, text=q_text)
