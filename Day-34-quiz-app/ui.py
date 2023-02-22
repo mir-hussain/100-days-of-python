a2844803f2fb550b7ef99e06a2434d06e52d2d84
Mir Hussain <mir.hm110@gmail.com>
2023-02-22 12:46:43 +0600

    score board added

--- Day-34-quiz-app/ui.py
+++ Day-34-quiz-app/ui.py
@@ -10,21 +10,31 @@ class QuizInterface():
         self.window.title("Quiz App")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
-        self.canvas = Canvas(width=300, height=250)
+        self.score = Canvas(width=100, height=50,
+                            bg=THEME_COLOR, highlightthickness=0)
+        self.score.create_text(50, 25, text="Score: ",
+                               font=("Aria", 18, "normal"), anchor="center")
+        self.score.grid(row=1, column=1, padx=0)
+
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0)
 
         self.canvas.create_text(150, 125, text="Test ",
                                 anchor="center", font=("Arial", 20, "normal"))
         # self.canvas.create_rectangle(0, 0, 300, 250, fill="white")
-        self.canvas.grid(row=1, column=1, columnspan=2, pady=20)
+        self.canvas.grid(row=2, column=1, columnspan=2, pady=20)
+
+        # * False Button
 
         false_button_image = PhotoImage(file="./images/false.png")
         self.false_button = Button(image=false_button_image)
 
-        self.false_button.grid(row=2, column=1, pady=20)
+        self.false_button.grid(row=3, column=1, pady=20)
+
+        # * True Button
 
         true_button_image = PhotoImage(file="./images/true.png")
         self.true_button = Button(image=true_button_image)
 
-        self.true_button.grid(row=2, column=2, pady=20)
+        self.true_button.grid(row=3, column=2, pady=20)
 
         self.window.mainloop()
