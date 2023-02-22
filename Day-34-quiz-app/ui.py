7be3aae31b589e83aaee5d410bba790d0cf04c80
Mir Hussain <mir.hm110@gmail.com>
2023-02-22 12:32:24 +0600

    buttons added

--- Day-34-quiz-app/ui.py
+++ Day-34-quiz-app/ui.py
@@ -15,6 +15,16 @@ class QuizInterface():
         self.canvas.create_text(150, 125, text="Test ",
                                 anchor="center", font=("Arial", 20, "normal"))
         # self.canvas.create_rectangle(0, 0, 300, 250, fill="white")
-        self.canvas.pack()
+        self.canvas.grid(row=1, column=1, columnspan=2, pady=20)
+
+        false_button_image = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=false_button_image)
+
+        self.false_button.grid(row=2, column=1, pady=20)
+
+        true_button_image = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=true_button_image)
+
+        self.true_button.grid(row=2, column=2, pady=20)
 
         self.window.mainloop()
