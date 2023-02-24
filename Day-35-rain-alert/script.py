0e37d733c285a8a1b97978fdd95c628d88972a06
mir-hussain <mir.hm110@gmail.com>
2023-02-24 15:34:48 +0600

    Day 35 rain alert added

--- Day-35-rain-alert/script.py
+++ Day-35-rain-alert/script.py
@@ -1,11 +1,17 @@
 import requests
 import os
+import smtplib
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MY_LAT = 23.777176
 MY_LONG = 90.399452
+EMAIL = os.getenv("email")
+PASSWORD = os.getenv("password")
+MAIL = ''
+print(EMAIL)
+print(PASSWORD)
 
 endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = os.getenv("key")
@@ -35,7 +41,19 @@ def check_for_rain():
     return rain
 
 
-print(check_for_rain())
+if check_for_rain():
+    MAIL = "It's going to rain today. Take your umbrella"
+else:
+    MAIL = "Chill, no rain in next 12 hours"
 
 
-# print(weather_data["hourly"][0]["weather"][0]["main"])
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=EMAIL,
+                     password=PASSWORD)
+    connection.sendmail(
+        from_addr=EMAIL,
+        to_addrs=EMAIL,
+        msg=f"Subject: Rain alert\n\n{MAIL}"
+    )
+    print(f"Mail send {MAIL}")
