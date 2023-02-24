ce1c80760aeeeec092494a8d8e047f6fd181baf7
mir-hussain <mir.hm110@gmail.com>
2023-02-24 19:24:50 +0600

    graph added

--- Day-37-github-graph-clone/script.py
+++ Day-37-github-graph-clone/script.py
@@ -4,14 +4,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token = os.getenv("token")
+USERNAME = "mir-hussain"
+TOKEN = os.getenv("token")
+
+pixela_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 user_info = {
-    "token": token,
-    "username": "mir-hussain",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
 # res = requests.post(url="https://pixe.la/v1/users", json=user_info)
 # print(res.text)
+
+graph_params = {
+    "id": "graph1",
+    "name": "Python Practice",
+    "unit": "day",
+    "type": "int",
+    "color": "ajisai",
+}
+
+res = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+print(res.text)
