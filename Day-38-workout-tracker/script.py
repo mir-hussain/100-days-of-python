57969e44b0689b18dc0b8b7b90bbfa7f14a352db
mir-hussain <mir.hm110@gmail.com>
2023-02-25 12:40:08 +0600

    initial post request added

--- Day-38-workout-tracker/script.py
+++ Day-38-workout-tracker/script.py
@@ -1,13 +1,29 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-app_key = os.getenv('app_key')
+api_key = os.getenv('api_key')
 app_id = os.getenv('app_id')
+
+endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 headers = {
     "x-app-id": app_id,
-    "x-app-key": app_key,
-    "Content-Type": "application/json"
+    "x-app-key": api_key,
 }
+
+print(headers)
+
+user_params = {
+    "query": "ran 3 miles",
+    "gender": "female",
+    "weight_kg": 72.5,
+    "height_cm": 167.64,
+    "age": 30
+}
+
+res = requests.post(url=endpoint, json=user_params, headers=headers)
+res.raise_for_status()
+print(res.json())
