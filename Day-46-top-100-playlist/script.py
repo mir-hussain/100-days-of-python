b2f21dcfd3c14a8b84a70b9223377c3a927b4a28
mir-hussain <mir.hm110@gmail.com>
2023-03-02 02:21:03 +0600

    spotify auth added

--- Day-46-top-100-playlist/script.py
+++ Day-46-top-100-playlist/script.py
@@ -1,15 +1,37 @@
-from bs4 import BeautifulSoup
+import os
 import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
-date = input("Enter the year you wanna travel to in this format YYYY-MM-DD ")
+load_dotenv()
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+redirect_uri = "http://example.com"
 
 
-res = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/")
-res.raise_for_status()
+scope = "user-library-read"
 
-soup = BeautifulSoup(res.text, "html.parser")
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
 
-title_list = soup.select("li ul li h3")
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " - ", track['name'])
 
-for title in title_list:
-    print(repr(title.text).replace("\\n", "").replace("\\t", ""))
+# date = input("Enter the year you wanna travel to in this format YYYY-MM-DD ")
+
+
+# res = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/")
+# res.raise_for_status()
+
+# soup = BeautifulSoup(res.text, "html.parser")
+
+# titles = soup.select("li ul li h3")
+
+# title_list = [title.get_text(strip=True) for title in titles]
+
+# print(title_list)
