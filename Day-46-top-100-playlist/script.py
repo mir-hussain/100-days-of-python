3d1131f86b8dc3d2b84d81d5c7277c2f3410ba25
mir-hussain <mir.hm110@gmail.com>
2023-03-02 21:23:40 +0600

    song uri scraping done

--- Day-46-top-100-playlist/script.py
+++ Day-46-top-100-playlist/script.py
@@ -4,34 +4,45 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import pprint
+
+pp = pprint.PrettyPrinter(depth=4)
 
 load_dotenv()
 
 client_id = os.getenv("client_id")
 client_secret = os.getenv("client_secret")
 redirect_uri = "http://example.com"
-
-
-scope = "user-library-read"
+scope = "playlist-modify-private"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " - ", track['name'])
+user_id = sp.current_user()["id"]
 
-# date = input("Enter the year you wanna travel to in this format YYYY-MM-DD ")
+date = input("Enter the year you wanna travel to in this format YYYY-MM-DD ")
+year = date.split("-")[0]
 
 
-# res = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/")
-# res.raise_for_status()
+res = requests.get(f"https://www.billboard.com/charts/hot-100/{date}/")
+res.raise_for_status()
 
-# soup = BeautifulSoup(res.text, "html.parser")
+soup = BeautifulSoup(res.text, "html.parser")
 
-# titles = soup.select("li ul li h3")
+titles = soup.select("li ul li h3")
 
-# title_list = [title.get_text(strip=True) for title in titles]
+title_list = [title.get_text(strip=True) for title in titles]
 
-# print(title_list)
+
+song_uris = []
+
+for title in title_list:
+
+    result = sp.search(q=f"track:{title} year:{year}", type="track")
+
+    try:
+        song_uris.append(result["tracks"]["items"][0]["uri"])
+    except IndexError:
+        print(f"{title} Doesn't Exist on Spotify. Skipped")
+
+sp.playlist()
