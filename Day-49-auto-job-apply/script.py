14f859d37df0d89523668467e5e94e171e4f324b
mir-hussain <mir.hm110@gmail.com>
2023-03-09 23:20:42 +0600

    job page added

--- Day-49-auto-job-apply/script.py
+++ Day-49-auto-job-apply/script.py
@@ -18,18 +18,21 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://www.linkedin.com/")
 
+time.sleep(1)
+
 username_el = driver.find_element(By.NAME, "session_key")
 username_el.send_keys(email)
-
-time.sleep(1)
 
 password_el = driver.find_element(By.NAME, "session_password")
 password_el.send_keys(password)
 
-time.sleep(1)
-
 driver.find_element(
     By.XPATH, '//*[@id="main-content"]/section[1]/div/form[1]/div[2]/button').click()
+
+time.sleep(2)
+
+driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
+
 
 while True:
     pass
