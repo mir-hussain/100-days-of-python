7712eddeaf2a4d9df017e9ec6a94aaee79bb2c63
mir-hussain <mir.hm110@gmail.com>
2023-03-31 13:03:35 +0600

    guess route added

--- Day-55-higher-lower/server.py
+++ Day-55-higher-lower/server.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello World</h1>"
+    return "<h1>Guess a number between 0 and 9</h1>" \
+        "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
+
+@app.route("/<int:guess>")
+def guess(guess):
+    return f'<h1>{guess}</h1>'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
